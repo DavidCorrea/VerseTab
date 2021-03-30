076e791928bb09f56b5fbf4929614af4aded7e0b
David Correa <davidjonatan.c@gmail.com>
2021-03-29 21:19:15 -0300

    Version 0.2

--- main.js
+++ main.js
@@ -1,48 +1,52 @@
-const bibleVersions = [ 
-  { code: 'kjv', name: 'KJV' }
-];
+/*
+possibleVersesToShow: [
+    '1:1',
+    '2:7',
+    '9:1',
+    '14:1',
+    '16:8',
+    '19:1',
+    '19:7',
+    '20:4',
+    '23:1',
+    '24:1',
+    '27:1',
+    '37:4',
+    '46:1',
+    '46:10',
+    '51:5',
+    '51:10',
+    '90:2',
+    '91:1',
+    '103:1',
+    '110:1',
+    '119:11',
+    '119:105',
+    '127:3',
+    '139:1',
+    '139:13',
+  ],
+  */
 
-const books = [ 
-  'psalms'
-];
+const randomFrom = list => list[Math.floor(Math.random() * list.length)];
 
-const chooseBibleAndBook = async () => {
-  const bibleVersion = bibleVersions[Math.floor(Math.random() * bibleVersions.length)];
-  const bookToUse = books[Math.floor(Math.random() * books.length)];
+const chooseBible = async () => {
+  const allBibles = (await import('./verses.js')).default;
+  const bible = randomFrom(allBibles);
 
-  const book = (await import(`./bibles/${bibleVersion.code}/${bookToUse}.js`)).default;
-
-  return { book, bibleVersion: bibleVersion.name }
+  return bible;
 };
 
-const chooseRandomVerseFromBook = ({ possibleVersesToShow }) => {
-  const rawVerse = possibleVersesToShow[Math.floor(Math.random() * possibleVersesToShow.length)];
-  const verseRegexp = rawVerse.match(/(?<chapterNumber>[0-9]*):(?<verseNumber>[0-9]*)/).groups;
-
-  return { chapterNumber: verseRegexp.chapterNumber, verseNumber: verseRegexp.verseNumber };
-};
-
-const selectChapter = ({ chapters }, { chapterNumber }) => {
-  return chapters.find((chapter) => chapter.chapter == chapterNumber);
-};
-
-const selectVerse = ({ verses }, { verseNumber }) => {
-  return verses.find((verse) => verse.verse == verseNumber);
-};
-
-const buildVerseDescription = (bibleVersion, { name: bookName }, { chapterNumber, verseNumber }) => {
-  return `${bookName} ${chapterNumber}:${verseNumber} - ${bibleVersion}`;
+const buildVerseDescription = ({ version }, { book, chapter, verse }) => {
+  return `${book} ${chapter}:${verse} - ${version}`;
 };
 
 const parseVerseToShow = async () => {
-  const { bibleVersion, book } = await chooseBibleAndBook();
+  const bible = await chooseBible();
+  const verse = randomFrom(bible.verses);
 
-  const verseLocation = chooseRandomVerseFromBook(book);
-  const chapter = selectChapter(book, verseLocation);
-  const { text } = selectVerse(chapter, verseLocation);
-
-  document.querySelector('.verse__content').innerHTML = text;
-  document.querySelector('.verse__details').innerHTML = buildVerseDescription(bibleVersion, book, verseLocation);
+  document.querySelector('.verse__content').innerHTML = verse.text;
+  document.querySelector('.verse__details').innerHTML = buildVerseDescription(bible, verse);
 };
 
 parseVerseToShow();
---
+++ verses.js
@@ -0,0 +1,96 @@
+export default [
+  {
+    "version": "KJV",
+    "verses": [
+      {
+        "book": "Psalms",
+        "chapter": 1,
+        "verse": 1,
+        "text": "Blessed is the man that walketh not in the counsel of the ungodly, nor standeth in the way of sinners, nor sitteth in the seat of the scornful."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 2,
+        "verse": 7,
+        "text": "I will declare the decree: the LORD hath said unto me, Thou art my Son; this day have I begotten thee."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 9,
+        "verse": 1,
+        "text": "I will praise thee, O LORD, with my whole heart; I will shew forth all thy marvellous works."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 14,
+        "verse": 1,
+        "text": "The fool hath said in his heart, There is no God. They are corrupt, they have done abominable works, there is none that doeth good."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 16,
+        "verse": 8,
+        "text": "I have set the LORD always before me: because he is at my right hand, I shall not be moved."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 19,
+        "verse": 1,
+        "text": "The heavens declare the glory of God; and the firmament sheweth his handywork."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 19,
+        "verse": 7,
+        "text": "The law of the LORD is perfect, converting the soul: the testimony of the LORD is sure, making wise the simple."
+      }
+    ]
+  },
+  {
+    "version": "NIV",
+    "verses": [
+      {
+        "book": "Psalms",
+        "chapter": 1,
+        "verse": 1,
+        "text": "Blessed is the one who does not walk in step with the wicked or stand in the way that sinners take or sit in the company of mockers,"
+      },
+      {
+        "book": "Psalms",
+        "chapter": 2,
+        "verse": 7,
+        "text": "I will proclaim the LORD’s decree: He said to me, “You are my son; today I have become your father."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 9,
+        "verse": 1,
+        "text": "I will give thanks to you, LORD, with all my heart; I will tell of all your wonderful deeds."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 14,
+        "verse": 1,
+        "text": "The fool says in his heart, “There is no God.” They are corrupt, their deeds are vile; there is no one who does good."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 16,
+        "verse": 8,
+        "text": "I keep my eyes always on the LORD. With him at my right hand, I will not be shaken."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 19,
+        "verse": 1,
+        "text": "The heavens declare the glory of God; the skies proclaim the work of his hands."
+      },
+      {
+        "book": "Psalms",
+        "chapter": 19,
+        "verse": 7,
+        "text": "The law of the LORD is perfect, refreshing the soul. The statutes of the LORD are trustworthy, making wise the simple."
+      }
+    ]
+  }
+]
